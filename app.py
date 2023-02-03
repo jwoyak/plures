241bc496d670258877ea761da03b18b2425a6ae1
Jason Woyak <jason@woyak.com>
2023-02-03 16:16:09 -0600

    added callbacks and file checking!

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ userHome = (os.environ['HOME'])
 # Build user's Chrome dir path
 chromeDir = (userHome + '/.config/google-chrome')
 
+# Build desktop file path for current user
+desktopPath = userHome + "/" + ".local/share/applications/"
+
 # Build path of localState
 localState = chromeDir + "/Local State"
  
@@ -31,8 +34,6 @@ user_name = ''
 profileName = ''
 ppPath = ''
 execLine = ''
-
-image=Gtk.Image()
 
 for e in os.listdir(chromeDir):
     dirEnt = os.path.join(chromeDir, e)
@@ -52,46 +53,25 @@ with open(localState) as file:
 js = json.loads(data)
 
 # Build the GUI
-listIndex=0
+listIndex=0     
 
-
-def profileListAction(*args):
-    print("Current Profile - ",listIndex)
-
-def hello_button_clicked(self, *args):
-    pass
-
-def on_tree_selection_changed(selection):
-    model, treeiter = selection.get_selected()
-    if treeiter is not None:
-        print("You selected", model[treeiter][0])
-        
-class profileList(Gtk.Window):
+class profileManager(Gtk.Window):
     def __init__(self):
         super().__init__(title="Profilium")
-        store = self.liststore = Gtk.ListStore(str,str)
-        treeview = Gtk.TreeView(store)
-        self.set_default_size(200, 200)
-        profileCol = Gtk.TreeViewColumn("Chrome Profile")
-        profileImage = Gtk.CellRendererPixbuf()
-        profileText = Gtk.CellRendererText()
-        profileCol.pack_start(profileImage, False)
-        profileCol.pack_start(profileText, True)
-        profileCol.add_attribute(profileText, "text", 0)
-        profileCol.add_attribute(profileImage, "pixbuf", 1)
-        
-        treeview.append_column(profileCol)    
+        self.set_border_width(10)
 
-        listIndex=0
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        vbox.set_homogeneous(False)
+
+        detailBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        detailBox.set_homogeneous(False)
+        detailBox.set_margin_top(20)
+
+        profileList = Gtk.ListStore(GdkPixbuf.Pixbuf, str, str)
+
         # for each profile in profilePaths, add to profileDic dictionary
 
-        #headerLabel = Gtk.Label(label=' Click on the button for the Chrome profile you want to manage. ')
-        #self.pack_start(headerLabel, True, True, 0)
-
         for profileName in profileNames:
-            listIndex = listIndex + 1
-
-            print("Processing profile " + profileName)
             profileDict = pd.json_normalize(js['profile']['info_cache'][profileName]).to_dict()
 
             # Get user_name
@@ -114,59 +94,112 @@ class profileList(Gtk.Window):
             # Build exec line for desktop entry
             execLine = "Exec='/usr/bin/google-chrome-stable %U --class=" + wmClass + " --user-data-dir='" + pfPath + "'"
 
-            # Build desktop file path for current user
-            desktopPath = userHome + "/" + ".local/share/applications/"
-
-            # Add rows in GUI
-            profImagePB = GdkPixbuf.Pixbuf
-            profImagePB.new_from_file(picPath)
-            #profImagePB = profImagePB.scale_simple(40, 40, 2) # 2 := BILINEAR
-            #profImage = image.new_from_pixbuf(profImagePB)
-            #image = GdkPixbuf.Pixbuf.new_from_file("images/icon.png")
             profileButtonLabel=' '+profileName+" - "+user_name
 
-            ### cell renderer
+            # Set the icon from the profile image
+            icon = GdkPixbuf.Pixbuf.new_from_file_at_size(picPath, 30, 30)
 
-            self.liststore.append([profImagePB, profileButtonLabel])
-
-            #detailLabel_name = Gtk.Label(label=execLine)
-            #self.pack_start(detailLabel_name, True, True, 0)
-
+            # Append row to the listStore
+            profileList.append([icon, user_name, profileName])
             
-        self.add(treeview)
-        select = treeview.get_selection()
-        select.connect("changed", on_tree_selection_changed)
 
+        treeview = Gtk.TreeView()
+        treeview.set_model(profileList)
+    
+        vbox.add(treeview)
+        vbox.add(detailBox)
 
+        self.add(vbox)
 
-win = profileList()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+        profilePixbuf = Gtk.CellRendererPixbuf()
 
-'''
+        profileImageCol = Gtk.TreeViewColumn("")
+        treeview.append_column(profileImageCol)
+        profileImageCol.pack_start(profilePixbuf, False)
+        profileImageCol.add_attribute(profilePixbuf, "pixbuf", 0)
+
+        accountUserName = Gtk.CellRendererText()
+
+        accountCol = Gtk.TreeViewColumn("Google Account")
+        treeview.append_column(accountCol)
+        accountCol.pack_start(accountUserName, True)
+        accountCol.add_attribute(accountUserName, "text", 1)
+        accountCol.set_sort_column_id(1)
+
+        profileDirName = Gtk.CellRendererText()
+
+        profileDirCol = Gtk.TreeViewColumn("Profile")
+        treeview.append_column(profileDirCol)
+        profileDirCol.pack_start(profileDirName, True)
+        profileDirCol.add_attribute(profileDirName, "text", 2)
+        profileDirCol.set_sort_column_id(2)
+
+        self.profileDetail = Gtk.Label()
+        self.profileDetail.set_text("")
+        self.profileDetail.set_halign(Gtk.Align.START)
+        detailBox.add(self.profileDetail)        
+        treeview_selection = treeview.get_selection()
+        treeview_selection.connect('changed', self.on_tree_selection_changed)
+
+    # Callback to handle getting/using list selection
+    def on_tree_selection_changed(self, profileSelection):
+        model, treeiter = profileSelection.get_selected()
+        if treeiter is not None:
+            # Read the selected user_name into selUser
+            selUser = (model[treeiter][1])
+
+            # Read the user's local application dir into localAppDir
+            localAppDir = desktopPath
+
+            # Check if the selected profile has a desktop entry
+            desktopFile = localAppDir + selUser + ".desktop"
+            if(os.path.exists(desktopFile)):
+                dfExists = True
+            else:
+                dfExists = False
+
+            # Build a multiline label to show all the info
+            selString = (
+                "Desktop Entry Path:\n" + localAppDir + "\n" +
+                "\n" +
+                "Selected Profile:\n" + selUser +" \n"
+                "\n" +
+                "Desktop File:\n" + str(desktopFile) + "\n"
+                "\n" +
+                "File Exists:\n" + str(dfExists) + "\n"
+                )
+
+            self.profileDetail.set_text(selString)
+
     # Build the desktop file in for each profile
-    with open(desktopPath+wmClass+"-profilium.desktop", "a") as f:
-        desktopFile = [
+    def buildDesktopFile(currentProfile):
+        desktopFilePath = profileManager.desktopPath+profileManager.wmClass+"-profilium.desktop"
+        with open(desktopFilePath, "a") as f:
+            desktopFile = [
             "Comment=Access the Internet\n",
-            execLine+'\n',
+            profileManager.execLine+'\n',
             "StartupNotify=true\n",
             "Terminal=false\n",
-            "Icon='"+picPath+"'\n",
+            "Icon='"+profileManager.picPath+"'\n",
             "Type=Application",
             "Categories=Network;WebBrowser;\n",
             "MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;\n",
             "Actions=new-window;new-private-window;\n",
-            "StartupWMClass="+wmClass+"\n",
+            "StartupWMClass="+profileManager.wmClass+"\n",
             "\n",
             "[Desktop Action new-window]\n",
             "Name=New Window\n",
-            execLine+'\n',
+            profileManager.execLine+'\n',
             "\n",
             "[Desktop Action new-private-window]\n",
             "Name=New Incognito Window\n",
             "Exec=/usr/bin/google-chrome-stable --incognito\n"
             ]
         f.writelines(desktopFile)
-        f.close()
-'''
+        f.close()        
+
+window = profileManager()
+
+window.show_all()
+
+Gtk.main()
